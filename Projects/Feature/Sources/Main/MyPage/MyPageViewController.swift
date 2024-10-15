d18d91b6adfab346d3b23930cb3a4a17d4f5d82d
heehee <leejh080628@naver.com>
2024-10-15 22:21:37 +0900

    feat :: label 추가 , view 자잘한 수정

--- Projects/Feature/Sources/Main/MyPage/MyPageViewController.swift
+++ Projects/Feature/Sources/Main/MyPage/MyPageViewController.swift
@@ -5,22 +5,56 @@ import SnapKit
 import Then
 
 public class MyPageViewController: BaseViewController {
+    private let customBackView = UIView().then {
+        $0.backgroundColor = .main1
+    }
     private let imagePicker = UIImagePickerController()
     private let imageView = UIImageView().then {
         $0.image = UIImage.imagePicker
         $0.isUserInteractionEnabled = true
     }
-
+    private let nameLabel = UILabel().then {
+        $0.text = "홍길동"
+        $0.font = .systemFont(ofSize: 26, weight: .semibold)
+    }
+    private let majorLabel = UILabel().then {
+        $0.text = "backend"
+        $0.font = .systemFont(ofSize: 12, weight: .medium)
+        $0.textColor = .textField
+    }
     public override func addView() {
-        view.addSubview(imageView)
+        [
+            customBackView,
+            imageView,
+            nameLabel,
+            majorLabel
+        ].forEach { view.addSubview($0) }
     }
 
     public override func layout() {
+        customBackView.snp.makeConstraints {
+            $0.top.equalTo(view.snp.top)
+            $0.leading.trailing.equalToSuperview()
+            $0.bottom.equalTo(view.snp.bottom).inset(744)
+            $0.width.equalTo(390)
+            $0.height.equalTo(100)
+        }
         imageView.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide).inset(27)
+//            $0.top.equalTo(view.safeAreaLayoutGuide).inset(27)
+            $0.bottom.equalTo(view.snp.bottom).inset(673)
             $0.leading.equalTo(20)
             $0.width.equalTo(106)
             $0.height.equalTo(100)
+        }
+        nameLabel.snp.makeConstraints {
+            $0.top.equalTo(imageView.snp.bottom).offset(9)
+            $0.leading.equalTo(20)
+            $0.trailing.equalTo(296)
+        }
+        majorLabel.snp.makeConstraints {
+            $0.top.equalTo(customBackView.snp.bottom).offset(94)
+            $0.leading.equalTo(100)
+            $0.trailing.equalTo(240)
         }
     }
     @objc func pickImage() {
@@ -28,9 +62,7 @@ public class MyPageViewController: BaseViewController {
     }
 
     public override func attribute() {
-        self.navigationController?.navigationBar.backgroundColor = .blue
         self.navigationItem.title = "마이페이지"
-        self.navigationController?.navigationBar.isTranslucent = false
         imagePicker.delegate = self
         imagePicker.sourceType = .photoLibrary
         imagePicker.allowsEditing = true
@@ -38,7 +70,6 @@ public class MyPageViewController: BaseViewController {
         imageView.addGestureRecognizer(tapGesture)
     }
 
-    
     public func presentImagePicker() {
         self.present(imagePicker, animated: true, completion: nil)
     }
