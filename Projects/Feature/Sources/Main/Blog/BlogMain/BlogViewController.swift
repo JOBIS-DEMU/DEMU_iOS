5240e3b09f1aac49ca3d097635e204e59733565b
jihoon <leejh080628@naver.com>
2024-10-21 02:05:15 +0900

    feat :: imagePicker 추가
    블로그 글 작성 뷰

--- Projects/Feature/Sources/Main/Blog/BlogMain/BlogViewController.swift
+++ Projects/Feature/Sources/Main/Blog/BlogMain/BlogViewController.swift
@@ -4,6 +4,55 @@ import Core
 import SnapKit
 import Then
 
-class BlogViewController: BaseViewController {
-//   private let 
+class BlogViewController: BaseViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+    private let cancelButton = UIButton().then {
+        $0.setTitle("취소", for: .normal)
+        $0.setTitleColor(.black, for: .normal)
+    }
+    private let checkButton = UIButton().then {
+        $0.setTitle("등록", for: .normal)
+        $0.setTitleColor(.black, for: .normal)
+    }
+    private let imagePicker = UIImagePickerController()
+    private let profileEditImageView = UIImageView().then {
+        $0.image = UIImage.profile
+        $0.layer.cornerRadius = 30
+        $0.layer.masksToBounds = true
+    }
+    public override func attribute() {
+        view.backgroundColor = .background
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = true
+    }
+    public override func addView() {
+        [
+            cancelButton,
+            checkButton,
+            profileEditImageView
+        ].forEach { view.addSubview($0) }
+    }
+    public override func layout() {
+        cancelButton.snp.makeConstraints {
+            $0.top.equalTo(62)
+            $0.leading.equalTo(24)
+        }
+        checkButton.snp.makeConstraints {
+            $0.top.equalTo(62)
+            $0.trailing.equalTo(-24)
+        }
+        profileEditImageView.snp.makeConstraints {
+            $0.top.equalTo(cancelButton.snp.bottom).offset(29)
+            $0.leading.equalTo(24)
+            $0.width.height.equalTo(50)
+        }
+    }
+    
+    @objc func pickImage() {
+        self.present(self.imagePicker, animated: true)
+    }
+    public func presentImagePicker() {
+        self.present(imagePicker, animated: true, completion: nil)
+    }
 }
+
