2ff91d8d263361e7441eb81656049f7746647a81
jihoon <leejh080628@naver.com>
2024-10-22 18:40:51 +0900

    feat :: modalCell 추가

--- Projects/Feature/Sources/Main/Blog/BlogMain/BlogViewController.swift
+++ Projects/Feature/Sources/Main/Blog/BlogMain/BlogViewController.swift
@@ -13,7 +13,6 @@ class BlogViewController: BaseViewController, UIImagePickerControllerDelegate & 
         $0.setTitle("등록", for: .normal)
         $0.setTitleColor(.black, for: .normal)
     }
-
     private let dropDownLabel = UILabel().then {
         $0.text = "backend"
         $0.font = .systemFont(ofSize: 24, weight: .semibold)
@@ -32,6 +31,12 @@ class BlogViewController: BaseViewController, UIImagePickerControllerDelegate & 
         $0.font = .systemFont(ofSize: 8, weight: .medium)
         $0.textColor = .gray
     }
+    private let imageSquareView = UIView().then {
+        $0.layer.cornerRadius = 5
+        $0.backgroundColor = .clear
+        $0.layer.borderWidth = 2
+        $0.layer.borderColor = UIColor.gray.cgColor
+    }
     public override func viewWillAppear(_ animated: Bool) {
          super.viewWillAppear(animated)
          navigationController?.setNavigationBarHidden(true, animated: false)
@@ -43,17 +48,23 @@ class BlogViewController: BaseViewController, UIImagePickerControllerDelegate & 
         imagePicker.allowsEditing = true
         self.navigationItem.hidesBackButton = true
         downButton.addTarget(self, action: #selector(presentBlogModal), for: .touchUpInside)
+        cancelButton.addTarget(self, action: #selector(cancelButtonTapped), for: .touchUpInside)
     }
     public override func addView() {
         [
             cancelButton,
             checkButton,
-            profileEditImageView,
             dropDownLabel,
             downButton,
-            numberCountLabel
+            imageSquareView
         ].forEach { view.addSubview($0) }
+
+        [
+            numberCountLabel,
+            profileEditImageView
+        ].forEach { imageSquareView.addSubview($0) }
     }
+
     public override func layout() {
         cancelButton.snp.makeConstraints {
             $0.top.equalTo(62)
@@ -63,11 +74,6 @@ class BlogViewController: BaseViewController, UIImagePickerControllerDelegate & 
             $0.top.equalTo(62)
             $0.trailing.equalTo(-24)
         }
-        profileEditImageView.snp.makeConstraints {
-            $0.top.equalTo(cancelButton.snp.bottom).offset(29)
-            $0.leading.equalTo(40)
-            $0.width.height.equalTo(18)
-        }
         dropDownLabel.snp.makeConstraints {
             $0.top.equalTo(58)
             $0.leading.equalTo(132)
@@ -76,9 +82,19 @@ class BlogViewController: BaseViewController, UIImagePickerControllerDelegate & 
             $0.top.equalTo(65)
             $0.leading.equalTo(242)
         }
+        imageSquareView.snp.makeConstraints {
+            $0.top.equalTo(113)
+            $0.leading.equalTo(24)
+            $0.width.height.equalTo(50)
+        }
         numberCountLabel.snp.makeConstraints {
-            $0.top.equalTo(145)
-            $0.leading.equalTo(42)
+            $0.top.equalTo(32)
+            $0.leading.trailing.equalTo(18)
+        }
+        profileEditImageView.snp.makeConstraints {
+            $0.top.equalTo(12)
+            $0.leading.equalTo(16)
+            $0.width.height.equalTo(18)
         }
     }
     @objc func presentBlogModal() {
@@ -86,6 +102,9 @@ class BlogViewController: BaseViewController, UIImagePickerControllerDelegate & 
         modalVC.modalPresentationStyle = .formSheet
         modalVC.modalTransitionStyle = .coverVertical
         self.present(modalVC, animated: true, completion: nil)
+    }
+    @objc func cancelButtonTapped() {
+        self.navigationController?.popViewController(animated: true)
     }
 
     @objc func pickImage() {
--- Projects/Feature/Sources/Main/Blog/BlogModal/BlogModalViewController.swift
+++ Projects/Feature/Sources/Main/Blog/BlogModal/BlogModalViewController.swift
@@ -5,24 +5,64 @@ import SnapKit
 import Then
 import RxSwift
 
-class BlogModalViewController: BaseViewController {
+class BlogModalViewController: BaseViewController, UITableViewDelegate, UITableViewDataSource {
+    private let tableView = UITableView()
+    let major = ["backend", "frontend", "iOS", "AOS", "AI", "design", "flutter", "full stack", "game", "security", "embedded", "devops", "기타 전공", "전체 글"]
+    private var selectedIndexPath: IndexPath?
+    private let backButton = UIButton().then {
+        $0.setImage(UIImage.back, for: .normal)
+    }
     private let titleLabel = UILabel().then {
         $0.text = "backend"
         $0.font = .systemFont(ofSize: 26, weight: .semibold)
         $0.textColor = .main1
     }
-    override func attribute() {
-        view.backgroundColor = .white
-    }
     public override func addView() {
         [
-            titleLabel
+            titleLabel,
+            tableView
         ].forEach { view.addSubview($0) }
+    }
+
+    override func attribute() {
+        view.backgroundColor = UIColor.background
+        tableView.register(BlogModalCell.self, forCellReuseIdentifier: "blogModalCell")
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.allowsMultipleSelection = false
+
     }
     public override func layout() {
         titleLabel.snp.makeConstraints {
             $0.top.equalToSuperview()
-            $0.leading.trailing.equalTo(153)
+            $0.centerX.equalToSuperview()
         }
-    }    
+        tableView.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(15)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(120)
+            $0.leading.trailing.equalToSuperview()
+        }
+    }
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return major.count
+    }
+
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "blogModalCell", for: indexPath) as? BlogModalCell else { return UITableViewCell() }
+        cell.selectionStyle = .none
+        cell.titleLabel.text = major[indexPath.row]
+        return cell
+    }
+
+    internal func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let previousIndexPath = selectedIndexPath {
+            guard let previousCell = tableView.cellForRow(at: previousIndexPath) as? BlogModalCell else { return }
+            previousCell.titleLabel.textColor = UIColor.textField
+        }
+        guard let cell = tableView.cellForRow(at: indexPath) as? BlogModalCell else { return }
+        cell.titleLabel.textColor = UIColor.main1
+        selectedIndexPath = indexPath
+        
+        titleLabel.text = major[indexPath.row]
+    }
 }
