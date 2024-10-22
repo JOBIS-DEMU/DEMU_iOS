e544889df4877c06b9afd5da671459d8f72174b4
jihoon <leejh080628@naver.com>
2024-10-22 16:40:02 +0900

    chore :: 주석 삭제

--- Projects/Feature/Sources/Main/TabBar/TabBarController.swift
+++ Projects/Feature/Sources/Main/TabBar/TabBarController.swift
@@ -1,11 +1,3 @@
-//
-//  TabBarVIewController.swift
-//  Feature
-//
-//  Created by 이지훈 on 10/14/24.
-//  Copyright © 2024 com.team.demu. All rights reserved.
-//
-
 import UIKit
 import SnapKit
 import Then
