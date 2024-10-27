46048ca1d2383e021a536a088dc4b6fbdb73ddac
jihoon <leejh080628@naver.com>
2024-10-27 17:05:17 +0900

    feat :: viewModel 추가

---
+++ Projects/Feature/Sources/Login/LoginViewModel.swift
@@ -0,0 +1,17 @@
+import Foundation
+import RxSwift
+import RxCocoa
+import Core
+
+class LoginViewModel: BaseViewModel {
+    private let disposeBag = DisposeBag()
+    
+    struct Input = <#type#>
+    
+    struct Output = <#type#>
+    
+   
+    func transform(input: Input) -> Output {
+        <#code#>
+    }
+}
