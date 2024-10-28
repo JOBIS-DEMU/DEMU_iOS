62d6c67cf48a1cc11632de80945e632a9505aef8
jihoon <leejh080628@naver.com>
2024-10-29 00:49:03 +0900

    feat :: ViewModelType

--- Projects/Feature/Sources/Auth/Api/AuthAPI.swift
+++ Projects/Feature/Sources/Auth/Api/AuthAPI.swift
@@ -3,4 +3,33 @@ import RxSwift
 import RxCocoa
 import Moya
 
+enum AuthAPI {
+    case signup(SignupInfo)
+    case login(id: String, password: String)
 
+}
+
+
+extension AuthAPI: TargetType {
+    var baseURL: URL {
+        return URL(string: "3.37.219.136:8080")!
+    }
+}
+
+var path: String {
+    switch self {
+    case .signup:
+        return "/public/signup"
+    case .login:
+        return "/public/login"
+        
+    }
+}
+
+
+var method: Moya.Method {
+case .
+default:
+    return .post
+    
+}
