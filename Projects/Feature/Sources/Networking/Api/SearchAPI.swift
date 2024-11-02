9295fb9d3fa4b676f473cf4cc140a8f8bd17911a
jihoon <leejh080628@naver.com>
2024-11-02 20:21:38 +0900

    feat :: searchAPI

---
+++ Projects/Feature/Sources/Networking/Api/SearchAPI.swift
@@ -0,0 +1,43 @@
+import Foundation
+import RxSwift
+import RxCocoa
+import Moya
+
+enum SearchAPI {
+    
+}
+
+extension SearchAPI: TargetType {
+    var baseURL: URL {
+        return URL(string: "http://3.37.219.136:8080")!
+    }
+    
+    
+    var path: String {
+        switch self {
+            
+        }
+    }
+    
+    
+    var method: Moya.Method {
+        switch self {
+        default:
+            return .get
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+            
+        }
+    default:
+        return .requestPlain
+    }
+    
+    var headers: [String : String]? {
+        switch self {
+            
+        }
+    }
+}
