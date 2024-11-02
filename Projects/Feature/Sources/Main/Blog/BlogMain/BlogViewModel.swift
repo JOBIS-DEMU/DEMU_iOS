4afc5dff547fe9710fc251ff48fc28dadc9d745b
circle0802 <hawon9781@gmail.com>
2024-11-02 14:22:20 +0900

    feat :: postservice, blogVM 추가

--- Projects/Feature/Sources/Main/Blog/BlogMain/BlogViewModel.swift
+++ Projects/Feature/Sources/Main/Blog/BlogMain/BlogViewModel.swift
@@ -1,8 +1,41 @@
-//
-//  BlogViewModel.swift
-//  Feature
-//
-//  Created by 원이 on 11/2/24.
-//  Copyright © 2024 com.team.demu. All rights reserved.
-//
+import Foundation
+import RxSwift
+import RxCocoa
 
+class BlogViewModel: ViewModelType {
+
+    private let disposeBag = DisposeBag()
+
+    struct Input {
+        let title: Driver<String>
+        let content: Driver<String>
+        let major: Driver<String>
+        let doneTap: Signal<Void>
+    }
+
+    struct Output {
+        let result: PublishRelay<Bool>
+    }
+
+    func transform(_ input: Input) -> Output {
+        let api = PostService()
+        let info = Driver.combineLatest(input.title, input.content, input.major)
+        let result = PublishRelay<Bool>()
+
+        input.doneTap.withLatestFrom(info).asObservable()
+            .flatMapLatest { title, content, major in
+                api.postCreate(title, content: content, major: major)
+            }
+            .subscribe(onNext: { res in
+                switch res {
+                    case .ok:
+                        result.accept(true)
+                    default:
+                        result.accept(false)
+                }
+            })
+            .disposed(by: disposeBag)
+        return Output(result: result)
+    }
+
+}
