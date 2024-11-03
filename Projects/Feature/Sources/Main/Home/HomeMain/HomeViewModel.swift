261c4bcf8b1754f3845a36100f30e9183d2f3f8f
jihoon <leejh080628@naver.com>
2024-11-03 15:44:26 +0900

    feat :: homeViewModel 수정

---
+++ Projects/Feature/Sources/Main/Home/HomeMain/HomeViewModel.swift
@@ -0,0 +1,24 @@
+import Foundation
+import RxSwift
+import RxCocoa
+
+class HomeViewModel: ViewModelType {
+    private let disposeBag = DisposeBag()
+    
+    
+    struct Input {
+        
+        
+    }
+    
+    
+    struct Output {
+        
+        
+    }
+    
+    func transform(_ input: Input) -> Output {
+        
+        
+    }
+}
