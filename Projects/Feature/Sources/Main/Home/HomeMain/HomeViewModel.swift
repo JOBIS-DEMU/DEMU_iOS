52fb24ef4e9df600127601116d7a6c855eb83857
jihoon <leejh080628@naver.com>
2024-11-03 21:36:16 +0900

    chore :: 주석처리

--- Projects/Feature/Sources/Main/Home/HomeMain/HomeViewModel.swift
+++ Projects/Feature/Sources/Main/Home/HomeMain/HomeViewModel.swift
@@ -1,40 +1,45 @@
-import Foundation
-import RxSwift
-import RxCocoa
-
-class HomeViewModel: ViewModelType {
-    private let disposeBag = DisposeBag()
-    
-    
-    struct Input {
-        let viewAppear: Signal<Void>
-        let refreshToken: Signal<Void>
-    }
-    
-    
-    struct Output {
-        let post: PublishRelay<PostModel>
-        let result: PublishRelay<Bool>
-        
-    }
-    
-    func transform(_ input: Input) -> Output {
-        let postAPI = PostService()
-        let authAPI = AuthService()
-        let popularPetition = PublishRelay<PostModel>()
-        let result = PublishRelay<Bool>()
-        
-        
-        input.viewAppear.asObservable()
-            .flatMap{ postAPI.loadPopularPetition() }
-            .subscribe(onNext: { data, res in
-                switch res {
-                    case .ok:
-                        popularPetition.accept(data!)
-                    default:
-                        return
-                }
-            }).disposed(by: disposeBag)
-
-    }
-}
+//import Foundation
+//import RxSwift
+//import RxCocoa
+//
+//class HomeViewModel: ViewModelType {
+//    func transform(_ input: Input) -> Output {
+//        <#code#>
+//    }
+//    
+//    private let disposeBag = DisposeBag()
+//    
+//    
+//    struct Input {
+//        let viewAppear: Signal<Void>
+//        let refreshToken: Signal<Void>
+//    }
+//    
+//    
+//    struct Output {
+//        let post: PublishRelay<PostModel>
+//        let result: PublishRelay<Bool>
+//        
+//    }
+//    
+//        func transform(_ input: Input) -> Output {
+//            let postAPI = PostService()
+//            let authAPI = AuthService()
+//            let popularPetition = PublishRelay<PostModel>()
+//            let result = PublishRelay<Bool>()
+//    
+//    
+//                    input.viewAppear.asObservable()
+//                        .flatMap{ postAPI.loadPopularPetition() }
+//                        .subscribe(onNext: { data, res in
+//                            switch res {
+//                                case .ok:
+//                                    popularPetition.accept(data!)
+//                                default:
+//                                    return
+//                            }
+//                        }).disposed(by: disposeBag)
+//            
+//                }
+//    
+//}
