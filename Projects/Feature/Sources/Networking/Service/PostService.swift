e67ef3ce21370b559f0c83d767b6156db97c84fb
jihoon <leejh080628@naver.com>
2024-11-03 20:48:01 +0900

    chore :: 수정

--- Projects/Feature/Sources/Networking/Service/PostService.swift
+++ Projects/Feature/Sources/Networking/Service/PostService.swift
@@ -42,9 +42,9 @@ final class PostService {
                 return .deleteOk
             }
     }
-    func loadPost() -> Single<(PostModel?, NetworkingResult)> {
-        
-    }
+//    func loadPost() -> Single<(PostModel?, NetworkingResult)> {
+//        
+//    }
 
     func setNetworkError(_ error: Error) -> NetworkingResult {
            print(error)
