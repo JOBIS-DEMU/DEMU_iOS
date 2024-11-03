8eb217a2295278e90bc39a620f59b452ec0e547a
jihoon <leejh080628@naver.com>
2024-11-03 15:54:31 +0900

    chore :: viewModel input 수정

--- Projects/Feature/Sources/Main/Home/HomeMain/HomeViewModel.swift
+++ Projects/Feature/Sources/Main/Home/HomeMain/HomeViewModel.swift
@@ -7,18 +7,34 @@ class HomeViewModel: ViewModelType {
     
     
     struct Input {
-        
-        
+        let viewAppear: Signal<Void>
+        let refreshToken: Signal<Void>
     }
     
     
     struct Output {
-        
+        let post: PublishRelay<PostModel>
+        let result: PublishRelay<Bool>
         
     }
     
     func transform(_ input: Input) -> Output {
+        let postAPI = PostService()
+        let authAPI = AuthService()
+        let popularPetition = PublishRelay<PostModel>()
+        let result = PublishRelay<Bool>()
         
         
+        input.viewAppear.asObservable()
+            .flatMap{ postAPI.loadPopularPetition() }
+            .subscribe(onNext: { data, res in
+                switch res {
+                    case .ok:
+                        popularPetition.accept(data!)
+                    default:
+                        return
+                }
+            }).disposed(by: disposeBag)
+
     }
 }
